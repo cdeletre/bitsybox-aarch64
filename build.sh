8481e434cc02627cc7b2fc4bb640ca0fdf008ab8
cdeletre <cyril.deletre@gmail.com>
2024-08-22 16:02:06 -0400

    Enable gcc optimizations

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 git clone https://github.com/le-doux/bitsybox.git bitsybox
 
-g++ bitsybox/src/bitsybox/main.c bitsybox/src/bitsybox/duktape/duktape.c `sdl2-config --libs` `sdl2-config --cflags` -lm -fpermissive -DPLATEFORM_LINUX -o bitsybox.aarch64
+g++ bitsybox/src/bitsybox/main.c bitsybox/src/bitsybox/duktape/duktape.c `sdl2-config --libs` `sdl2-config --cflags` -lm -fpermissive -DPLATEFORM_LINUX -O2 -o bitsybox.aarch64
